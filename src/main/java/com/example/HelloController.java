a17d4f38d73313da4b0f2d61ab7091811ce2a812
mdd3135 <313521996@qq.com>
2022-08-02 11:06:00 +0800

    update

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
